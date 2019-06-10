0f67b30554fea5875758c7abbbf33e2c7c766d87
askmeegs <megan037@gmail.com>
2019-06-10 11:50:51 -0400

    update repo url with ssh prefix

--- config-management/qwiklab/setup-repo.sh
+++ config-management/qwiklab/setup-repo.sh
@@ -15,34 +15,35 @@
 export PROJECT=$(gcloud config get-value project)
 
 # repo 
-# cd $HOME
+cd $HOME
 export GCLOUD_ACCOUNT=$(gcloud config get-value account)
-export REPO_URL=https://source.developers.google.com/p/${PROJECT}/r/config-repo
+export REPO_URL=ssh://${GCLOUD_ACCOUNT}@source.developers.google.com:2022/p/${PROJECT}/r/config-repo
 
-# git clone https://github.com/cgrant/config-repo config-repo
-# cd config-repo
-# git remote remove origin
-# git config credential.helper gcloud.sh
-# git remote add origin $REPO_URL
 
-# gcloud source repos create config-repo
-# git push -u origin master
+git clone https://github.com/cgrant/config-repo config-repo
+cd config-repo
+git remote remove origin
+git config credential.helper gcloud.sh
+git remote add origin $REPO_URL
 
-# # give ACM permission to read the repo 
-# ssh-keygen -t rsa -b 4096 \
-# -C "$GCLOUD_ACCOUNT" \
-# -N '' \
-# -f $HOME/.ssh/id_rsa.nomos
+gcloud source repos create config-repo
+git push -u origin master
 
-# # apply token to the cluster (using central only) 
-# kubectx central
-# kubectl create secret generic git-creds \
-# --namespace=config-management-system \
-# --from-file=ssh=$HOME/.ssh/id_rsa.nomos
+# give ACM permission to read the repo 
+ssh-keygen -t rsa -b 4096 \
+-C "$GCLOUD_ACCOUNT" \
+-N '' \
+-f $HOME/.ssh/id_rsa.nomos
 
-# # user needs key to register to GCR 
-# echo "-------- YOUR SSH KEY ----------- "
-# cat $HOME/.ssh/id_rsa.nomos.pub
+# apply token to the cluster (using central only) 
+kubectx central
+kubectl create secret generic git-creds \
+--namespace=config-management-system \
+--from-file=ssh=$HOME/.ssh/id_rsa.nomos
+
+# user needs key to register to GCR 
+echo "-------- YOUR SSH KEY ----------- "
+cat $HOME/.ssh/id_rsa.nomos.pub
 
 # configure ACM to look for files in this repo 
 kubectx $CENTRAL
