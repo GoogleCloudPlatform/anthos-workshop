cb997f6568df15eae80c89f3c95c160dee37fe63
askmeegs <megan037@gmail.com>
2019-09-13 09:01:25 -0400

    cleanup

--- gke/provision-gke.sh
+++ gke/provision-gke.sh
@@ -34,7 +34,7 @@ gcloud beta container clusters create $CLUSTER_NAME --zone $CLUSTER_ZONE \
     --addons=HorizontalPodAutoscaling,HttpLoadBalancing,Istio,CloudRun \
     --istio-config=auth=MTLS_PERMISSIVE \
     --username "admin" \
-    --machine-type "n1-standard-4" \
+    --machine-type "n1-standard-8" \
     --image-type "COS" \
     --disk-size "100" \
     --scopes "https://www.googleapis.com/auth/compute","https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
--- service-mesh/enable-service-mesh.sh
+++ service-mesh/enable-service-mesh.sh
@@ -23,20 +23,19 @@ ACCOUNT=$(gcloud config get-value account)
 kubectl create clusterrolebinding cluster-admin-binding --clusterrole="cluster-admin" --user=${ACCOUNT}
 
 MESH_ID="${PROJECT_ID}/${ZONE}/${CLUSTER}"
-gsutil cat gs://csm-artifacts/stackdriver/stackdriver.istio_1_1.csm_beta.yaml | \
-sed 's@<mesh_uid>@'${MESH_ID}@g | kubectl apply -f -
+
+gsutil cat gs://csm-artifacts/stackdriver/stackdriver.istio_1_1.csm_beta.yaml | sed 's@<mesh_uid>@'${MESH_ID}@g | kubectl apply -f -
 
 gcloud iam service-accounts create istio-mixer --display-name istio-mixer --project ${PROJECT_ID}
 
 gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=serviceAccount:istio-mixer@${PROJECT_ID}.iam.gserviceaccount.com --role=roles/contextgraph.asserter
-
 gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=serviceAccount:istio-mixer@${PROJECT_ID}.iam.gserviceaccount.com --role=roles/logging.logWriter
-
 gcloud projects add-iam-policy-binding ${PROJECT_ID} --member=serviceAccount:istio-mixer@${PROJECT_ID}.iam.gserviceaccount.com --role=roles/monitoring.metricWriter
-
 gcloud iam service-accounts add-iam-policy-binding --role roles/iam.workloadIdentityUser --member "serviceAccount:${PROJECT_ID}.svc.id.goog[istio-system/istio-mixer-service-account]" istio-mixer@${PROJECT_ID}.iam.gserviceaccount.com
 
 kubectl annotate serviceaccount --namespace istio-system istio-mixer-service-account iam.gke.io/gcp-service-account=istio-mixer@${PROJECT_ID}.iam.gserviceaccount.com
+
+kubectl label namespace default istio-injection=enabled
 
 kubectl delete po $(kubectl get pod -n istio-system -l app=telemetry -o json | jq -r '.items[].metadata.name') -n istio-system
 
