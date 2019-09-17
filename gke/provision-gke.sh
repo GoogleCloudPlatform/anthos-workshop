4722aa120c5b8dc62e62a0157d875a253d4766d2
Andre Idalgo Rodrigues <airodrigues@google.com>
2019-09-17 16:03:02 -0500

    Moved all tools versions to env file (kubectx, kops, gke, helm, istio).

--- gke/provision-gke.sh
+++ gke/provision-gke.sh
@@ -18,7 +18,6 @@
 export PROJECT=$(gcloud config get-value project)
 export WORK_DIR=${WORK_DIR:="${PWD}/workdir"}
 
-export CLUSTER_VERSION="1.12"
 export CLUSTER_NAME="central"
 export CLUSTER_ZONE="us-central1-b"
 export CLUSTER_KUBECONFIG=$WORK_DIR/central.context
