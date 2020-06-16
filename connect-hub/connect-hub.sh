163ea4aaa86f08336a8a13abc1be1b7f4e43f73e
Christopher Grant <cgrant@users.noreply.github.com>
2020-06-16 11:33:48 -0500

    Updating hub memberships to GA command (#62)
    Co-authored-by: crgrant <crgrant@google.com>

--- connect-hub/connect-hub.sh
+++ connect-hub/connect-hub.sh
@@ -57,11 +57,11 @@ gcloud iam service-accounts keys create $GKE_SA_CREDS --project=$PROJECT \
 
 
 ### Connect Cluster to Hub
-gcloud beta container memberships register $REMOTE_CLUSTER_NAME_BASE\
- --context=$REMOTE_CLUSTER_NAME \
- --service-account-key-file=$GKE_SA_CREDS \
- --kubeconfig=$REMOTE_KUBECONFIG \
- --project=$PROJECT
+gcloud container hub memberships register $REMOTE_CLUSTER_NAME_BASE \
+  --project=$PROJECT \
+  --context=$REMOTE_CLUSTER_NAME \
+  --kubeconfig=$REMOTE_KUBECONFIG \
+  --service-account-key-file=$GKE_SA_CREDS 
 
 
 ## Create Service Account for use in Login Step
