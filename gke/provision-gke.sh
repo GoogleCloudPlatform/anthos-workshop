0d22d036305712040b573448a48f0e24bbc249d9
Sergey Kanzhelev <S.Kanzhelev@live.com>
2021-04-28 17:42:13 -0700

    Update provision-gke.sh

--- gke/provision-gke.sh
+++ gke/provision-gke.sh
@@ -29,6 +29,7 @@ else
     echo "Creating cluster..."
     gcloud beta container clusters create ${CLUSTER_NAME} --zone ${CLUSTER_ZONE} \
         --username "admin" \
+        --machine-type "n1-standard-4" \        
         --scopes "https://www.googleapis.com/auth/compute","https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
         --num-nodes "2" \
         --enable-autoscaling --min-nodes 2 --max-nodes 8 \
